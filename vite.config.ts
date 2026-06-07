ad5b6047adaf6d1f5b834d83639d04bf6c285005
siev0000 <skkt36@gmail.com>
2026-06-07 22:19:27 +0900

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const base = repositoryName ? `/${repositoryName}/` : '/';
-
 export default defineConfig({
-  base,
+  base: '/novel-writing-tool-starter/',
   plugins: [vue()],
 });
